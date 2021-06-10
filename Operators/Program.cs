af3491d1b40731124855c80c6d057451219d7d15
Kağan <kagansaygin@gmail.com>
2021-06-10 09:25:09 +0300

    Operatorler

---
+++ Operators/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            y *= 2;
+            Console.WriteLine(y);
+            
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfect");
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Good");
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine");
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1++;
+            Console.WriteLine(sonuc1);
+        }
+    }
+}
