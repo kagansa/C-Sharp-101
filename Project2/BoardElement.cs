166ad67f7a8ad826b57154396c45922689b748c1
Kağan <kagansaygin@gmail.com>
2021-06-10 10:33:10 +0300

    PROJE-2
    Console ToDo Uygulaması

---
+++ Project2/BoardElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Project2
+{
+    public class BoardElement
+    {
+        public Card Card { get; set; }
+        public StatusType Status { get; set; }
+
+        public BoardElement(Card card, StatusType status)
+        {
+            this.Card = card;
+            this.Status = status;
+        }
+
+        public BoardElement()
+        {
+        }
+    }
+}
---
+++ Project2/DataSource/MemberDataSource.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Project2.DataSource
+{
+    public static class MemberDataSource
+    {
+        public static List<Member> Members;
+
+        static MemberDataSource()
+        {
+            Members = InitializeMembers();
+        }
+
+        private static List<Member> InitializeMembers()
+        {
+            return new List<Member>()
+            {
+                new Member(1,"Kağan Saygın"),
+                new Member(2,"Ayşe Taş"),
+                new Member(3,"Fatma Demir"),
+                new Member(4,"Hayriye Yılmaz")
+            };
+        }
+    }
+}
