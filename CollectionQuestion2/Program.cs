9d21a3f66dc4d55f98c8deb69c6dc03b15be8177
Kağan <kagansaygin@gmail.com>
2021-06-10 10:29:29 +0300

    Koleksiyon Soru 2

---
+++ CollectionQuestion2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionQuestion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            int counter = 0;
+
+            while (counter < 20)
+            {
+                Console.WriteLine($"{counter + 1}. Sayıyı Giriniz.");
+
+                int value = int.Parse(Console.ReadLine());
+
+                if (value <= 0)
+                {
+                    Console.WriteLine("Sayı Sıfırdan Küçük Olamaz.");
+                    continue;
+                }
+
+                numbers.Add(value);
+                counter++;
+
+                Console.WriteLine($"En Büyük 3 Sayı : {string.Join(",", numbers.OrderByDescending(x => x).Take(3))}");
+                Console.WriteLine($"En Küçük 3 Sayı : {string.Join(",", numbers.OrderBy(x => x).Take(3))}");
+                Console.WriteLine($"En Büyük 3 Sayının Ortalaması : {string.Join(",", numbers.OrderByDescending(x => x).Take(3).Average())}");
+                Console.WriteLine($"En Küçük 3 Sayının Ortalaması : {string.Join(",", numbers.OrderBy(x => x).Take(3).Average())}");
+
+            }
+        }
+    }
+}
