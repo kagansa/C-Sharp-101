fa4bb723b45f59df11700244f0e951ea982a00f7
Kağan <kagansaygin@gmail.com>
2021-06-10 10:32:31 +0300

    PROJE-1
    Console Telefon Rehberi Uygulaması

---
+++ Project1/PersonDataSource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Project1
+{
+    public static class PersonDataSource
+    {
+        public static List<Person> persons;
+
+
+        static PersonDataSource()
+        {
+            persons = InitializePersons();
+        }
+
+
+        static List<Person> InitializePersons()
+        {
+            return new List<Person>()
+            {
+                new Person("Kağan","Saygın","05464182562"),
+                new Person("Ayşe","Yılmaz","05455451284"),
+                new Person("Fatma","Taş","05423547651"),
+                new Person("Hayriye","Demir","05455466789"),
+                new Person("Eren","Yılmaz","05412234050")
+            };
+
+        }
+    }
+}
